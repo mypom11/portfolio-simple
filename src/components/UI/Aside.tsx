e34c554bd3da12fabc029ab1f21bc984fb8134fa
jeongki Min <mypom11@gmail.com>
2023-09-12 12:27:51 +0900

    aside 오류 수정

--- src/components/UI/Aside.tsx
+++ src/components/UI/Aside.tsx
@@ -1,42 +1,42 @@
 const nav = [
   {
     name: '프로필',
-    link: '1',
+    id: 1,
   },
   {
     name: '기술 스택',
-    link: '2',
+    id: 2,
   },
   {
     name: '개인 프로젝트',
-    link: '3',
+    id: 3,
   },
   {
     name: '경력 사항',
-    link: '4',
+    id: 4,
   },
   {
     name: '회사 프로젝트',
-    link: '5',
+    id: 5,
   },
 
   {
     name: '교육 및 활동',
-    link: '6',
+    id: 6,
   },
 ]
 
 interface liComponent {
   name: string
-  link: string
+  id: number
 }
 
-const AnimationLi = ({ name, link }: liComponent) => {
+const AnimationLi = ({ name, id }: liComponent) => {
   return (
     <li className="cursor-pointer li-ani text-xl">
       {name.split('').map((word, index) => (
         <span
-          key={`${link}-${word}` + index}
+          key={`${id}_${index}`}
           className={`inline-block ${word === ' ' ? 'pl-1' : ''}`}
         >
           {word}
@@ -56,8 +56,8 @@ const Aside: React.FC<{ isNav: boolean }> = ({ isNav }) => {
     >
       <nav className="relative p-12">
         <ul className="flex-col flex gap-8 text-lg font-medium">
-          {nav.map((item, index) => (
-            <AnimationLi key="index" name={item.name} link={item.link} />
+          {nav.map((item) => (
+            <AnimationLi key={item.id} name={item.name} id={item.id} />
           ))}
         </ul>
       </nav>
